7ec0746fe136b72cf33aa1ee1367019e538bddbc
Alejo Arias <alejo12arias@gmail.com>
2023-09-14 03:04:10 -0300

    Mejorando el oral

--- MisPracticasWEB/Oral-DW-Lunes18/js/login.js
+++ MisPracticasWEB/Oral-DW-Lunes18/js/login.js
@@ -1,12 +1,15 @@
+//CODIGO PARA REGISTRARSE
 let registerBtn = document.getElementById('register');
+
 
 //EVENTO QUE AL HACER CLICK EN "REGISTRO" VERIFICA LOS CAMPOS DE TEXTO Y SI EL EMAIL CONTIENE ARROBA
 registerBtn.addEventListener('click', function () {
 
-    let name = document.getElementById('name-login').value;
-    let ape = document.getElementById('ape-login').value;
-    let email = document.getElementById('email-login').value;
-    let pass = document.getElementById('pass-login').value;
+    let name = document.getElementById('name-register').value;
+    let ape = document.getElementById('ape-register').value;
+    let email = document.getElementById('email-register').value;
+    let pass = document.getElementById('pass-register').value;
+   
 
     if (name == '' || ape == '' || email == '' || pass == '') {
         alert("Erorr, Complete todo el formulario");
@@ -18,7 +21,17 @@ registerBtn.addEventListener('click', function () {
     } else {
         window.location.href = "paginas/gerarditoGPT.html"
         alert("Formulario Correcto, Redirecionando a GerarditoGPT....")
+        
+        //GUARDAR VALORES
+        localStorage.setItem("email", email);
+        localStorage.setItem("pass", pass);
+
         return true;
     }
     
 });
+
+function logRegister() {
+    window.location.href = "/paginas/login.html";
+    alert("Redirecionando a Iniciar Seccion...")
+}
